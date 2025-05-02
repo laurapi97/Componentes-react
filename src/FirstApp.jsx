797799e76bcbf29353b9b5fb3a0b02f1abd110e4
Laura Fiona <laurapinzonm7@gmail.com>
2025-05-02 11:47:31 -0500

    Añadiendo hooks al counter

--- src/FirstApp.jsx
+++ src/FirstApp.jsx
@@ -1,48 +1,36 @@
-// import {Fragment} from 'react';
+
+import PropTypes from 'prop-types'; // proptypes es una libreria que nos permite validar los tipos de las propiedades que le pasamos a un componente
 
 
-// export const FirstApp = () => {
-//     return (
-//         // EL Fragment es un contenedor que no se renderiza en el DOM
-//         // Se utiliza para agrupar varios elementos sin añadir un nodo extra al DOM
-//         // En este caso, se utiliza para agrupar el h1 y el p
-//         // El Fragment se puede usar de dos maneras: <Fragment> o <></>
-//         // En vez del div, se puede usar un Fragment para evitar añadir un nodo extra al DOM
-//         // Se ve como <div id="root"></div> en el DOM, pero no se ve en el HTML
-//        <Fragment>
-//         <h1>Fiona</h1>
-//         <p>Soy una gatita gris</p>
-//         </Fragment>
-//     );
+// alternativa al defaultProps, ya que defaultProps sera descontinuado en el futuro
+export const FirstApp = ({ title ='No hay título', subTitle = 'No hay subtítulo', nombre ='No hay nombre'}) => {
+
+    //    console.log(props);
+    // <> es un fragmento de codigo que nos permite agrupar varios elementos sin necesidad de crear un div
+        return (
+            <>
+                <h1>{title}</h1>   
+                {/* <h1>{getResult()}</h1>  */}
+                {/* <code>{ JSON.stringify(newMessage) }</code>  */}
+                <p>{subTitle}</p>
+                <p>{nombre}</p>
+            </>
+        )
+    }
+
+
+// proptypes es una libreria que nos permite validar los tipos de las propiedades que le pasamos a un componente
+// PropTypes es un objeto que contiene los tipos de las propiedades que le pasamos a un componente
+FirstApp.propTypes = {
+   title: PropTypes.string,
+   subTitle: PropTypes.string,
+   nombre: PropTypes.string, // nombre es una propiedad que le pasamos al componente FirstApp
+}  
+
+// defaultProps sera descontinuado en el futuro, por lo que se recomienda usar proptypes para definir los tipos de las propiedades que le pasamos a un componente
+// FirstApp.defaultProps = {
+//     title: 'No hay titulo', // si no le pasamos un titulo, se le asignara este valor por defecto
+//     subTitle: 'No hay subtitulo', // si no le pasamos un subtitulo, se le asignara este valor por defecto
 // }
 
 
-
-//  Fragment es un componente de React que permite agrupar varios elementos sin añadir un nodo extra al DOM
-// <> es una forma abreviada de usar Fragment, y se utiliza para agrupar varios elementos sin añadir un nodo extra al DOM
-// En este caso, se utiliza para agrupar el h1 y el p
-
-// const getResult =() =>{
-//     return 100 + 200;
-// }
-
-
-
-// const newMessage = {
-//     nombre :'Fiona',
-//     edad: '10',
-// };
-
-export const FirstApp = ({ title = 'Hola soy Fiona', subTitle}) => {
-
-
-//    console.log(props);
-    return (
-        <>
-            <h1>{title}</h1>   
-            {/* <h1>{getResult()}</h1>  */}
-            {/* <code>{ JSON.stringify(newMessage) }</code>  */}
-            <p>{subTitle + 1}</p>
-        </>
-    )
-}
--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,9 @@
 import React from 'react'   
 import ReactDOM from 'react-dom/client'
 // import { HelloWorldApp } from './HelloWorldApp'
-import { FirstApp } from './FirstApp'
+// import { FirstApp } from './FirstApp'
 import './styles.css'
+import { CounterApp } from './CounterApp'
 // import App from './HelloWorldApp.jsx' // Importamos el componente App'
 // Importamos el componente HelloWorldApp
 
@@ -16,6 +17,9 @@ import './styles.css'
 ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
         {/* Padre */}
-        <FirstApp  title ="Hola, soy Fiona" subTitle = {123}/>   
+        {/* <FirstApp title ="Hola soy Fiona" nombre ="Fionita la mas hermosa"/>    */}
+        {/* <FirstApp /> */}
+        <CounterApp value ={10} />
+        
     </React.StrictMode>
 )
